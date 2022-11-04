27daddf9b8c9a1ca2f0b61a1218ffa76caee0957
Dzure <straydogs345@gmail.com>
2022-11-04 16:43:24 +0700

    .
    .

---
+++ Server.java
@@ -0,0 +1,35 @@
+
+
+import java.util.Scanner;
+
+public class Server {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String pilihanmenu,menu,exit;
+        System.out.print("========================== \n");
+        System.out.print("1.Menu \n");
+        System.out.print("2.Pilih Menu \n");
+        System.out.print("3.Keluar\n ");
+        
+
+
+
+
+
+
+
+        // TODO Create Database
+
+        // TODO Create Menu
+
+        // TODO Insert Menu to Database
+
+
+        // TODO Display Main Menu
+
+        // TODO Create User
+
+        // Display Menu
+    }
+}
---
+++ User.java
@@ -0,0 +1,24 @@
+
+public class User {
+    String NamaUser;
+    int Phone;
+
+    public void setNamaUser(String NamaUser){
+        this.NamaUser = NamaUser;
+    }
+
+    public void setCategory(int Phone){
+        this.Phone = Phone;
+    }
+
+
+
+
+
+
+    // TODO Create Attribute of User; Name and Phone Number then Create Setter
+
+    // TODO Create Method to Register User and Display User's Name and Phone Number and success message
+
+
+}
